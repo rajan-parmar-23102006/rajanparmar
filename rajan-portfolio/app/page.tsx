c32e91a130688c864d62ab653a907b58258f3129
rajan parmar <rajan76858@gmail.com>
2025-12-18 23:29:14 +0530

    add animaion for starting app

--- rajan-portfolio/app/page.tsx
+++ rajan-portfolio/app/page.tsx
@@ -7,10 +7,12 @@ import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
+import LoadingScreen from "@/components/LoadingScreen";
 
 export default function Home() {
   return (
     <>
+      <LoadingScreen /> {/* ← Add this FIRST */}
       <Navbar />
       <Hero />
       <About />
