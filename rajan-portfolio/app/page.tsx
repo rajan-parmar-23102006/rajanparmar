815d64168dd7619e624ec8c973a4eb02b6b63c78
rajan parmar <rajan76858@gmail.com>
2025-12-18 23:16:56 +0530

    remove animatins

--- rajan-portfolio/app/page.tsx
+++ rajan-portfolio/app/page.tsx
@@ -7,23 +7,19 @@ import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
-import StarryBackground from "@/components/StarryBackground"; // ← Add this
 
 export default function Home() {
   return (
     <>
-      <StarryBackground /> {/* ← Add this FIRST */}
-      <div className="relative z-10"> {/* ← Wrap all content */}
-        <Navbar />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Experience />
-        <Contact />
-        <Footer />
-        <ScrollToTop />
-      </div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
+      <Footer />
+      <ScrollToTop />
     </>
   );
 }
